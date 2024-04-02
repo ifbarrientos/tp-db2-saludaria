1778f0a26cb3eaa9b8e6791af505129fc47fdaa3
Ignacio Barrientos <ifbarrientos@outlook.com>
2024-04-02 00:49:07 -0300

    Prueba primer commit

---
+++ punto2.sql
@@ -0,0 +1,25 @@
+drop database if exists centro;
+CREATE DATABASE centro;
+
+USE centro;
+
+-- Creación de tablas
+
+create table paciente (nombre char(16), apellido char(16), dni int, telefono char(12), mail char(20));
+
+create table obra_social (nombre char(16), cuit int, plan enum);
+
+create table turno (fecha date, estado enum);
+
+create table medico (nombre char(16), apellido char(16), dni int, telefono char(12));
+
+create table servicio_medico (nombre char(20), id int);
+
+create table equipo_medico (nombre char(20), id int);
+
+create table agenda (disponible boolean, franja_inicio date, franja_fin date);
+
+create table centro (nombre char(20), direccion char(20), id int);
+
+-- Inserts...
+
