9b959d5165efbf208758a849e2be037823835883
Ignacio Barrientos <ifbarrientos@outlook.com>
2024-04-02 19:59:28 -0300

    Se completaron los create e inserts

--- punto2.sql
+++ punto2.sql
@@ -13,13 +13,53 @@ create table turno (fecha date, estado ENUM('confirmado', 'reservado', 'cancelad
 
 create table medico (nombre char(16), apellido char(16), dni int, telefono char(12));
 
-create table servicio_medico (nombre char(20), id int);
+create table servicio_medico (nombre ENUM(
+    'Ecografia', 'Tomografia', 'Radiologia'
+), id int);
 
 create table equipo_medico (nombre char(20), id int);
 
-create table agenda (disponible boolean, franja_inicio date, franja_fin date);
+create table agenda (franja_inicio DATETIME, franja_fin DATETIME);
 
-create table centro (nombre char(20), direccion char(20), id int);
+create table centro (nombre char(20), direccion char(20), telefono char(12), id int);
 
 -- Inserts...
 
+insert into paciente('Ignacio', 'Barrientos', 42947954, '1164925997', 'ifbarrientos@outlook.com');
+
+insert into paciente('John', 'Doe', 123456789, '1125746963', 'doejohn@gmail.com');
+
+
+insert into obra_social('Galeno', 2042947954, 'estandar');
+
+insert into obra_social ('OSDE', 5012345678900, 'oro');
+
+
+insert into turno ('2024/04/12', 'reservado');
+
+insert into turno ('2024/04/15', 'cancelado');
+
+
+insert into medico ('Jane', 'Doe', 987654321, '1167983554');
+
+insert into medico ('Luis', 'Lopez', 628447806, '1164352964');
+
+
+insert into servicio_medico ('Ecografia', 1);
+
+insert into servicio_medico ('Tomografia', 2);
+
+insert into servicio_medico ('Radiologia', 3);
+
+
+insert into equipo_medico ('Radiologia_1', 4);
+
+insert into equipo_medico ('Tomografia_1', 5);
+
+insert into equipo_medico ('Ecografia_1', 6);
+
+
+insert into agenda ('2024/04/12 08:00:00', '2024/04/12 14:00:00');
+insert into agenda ('2024/04/15 08:00:00', '2024/04/12 14:00:00');
+
+insert into centro ('Sede Malvinas Argentinas', 'Jose María Gütierrez 1150', 1168476247, 1);
